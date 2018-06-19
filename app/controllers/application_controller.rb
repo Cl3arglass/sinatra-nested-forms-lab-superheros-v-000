ff209133bef5573404f88e3294b76bcbcbe0f00f
Holden Altaffer <haltaffer@gmail.com>
2018-06-19 00:22:25 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,18 +9,18 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
-      # @team = Team.new(params[:team][:name], params[:team][:motto])
-      #
-      #
-      # Member.new(params[:team][:members][0][:name], params[:team][:members][0][:power], params[:team][:members][0][:bio])
-      # Member.new(params[:team][:members][1][:name], params[:team][:members][1][:power], params[:team][:members][1][:bio])
-      # Member.new(params[:team][:members][2][:name], params[:team][:members][2][:power], params[:team][:members][2][:bio])
-      #
-      # @members = Member.all
-      #
-      # erb :team
+      @team = Team.new(params[:team][:name], params[:team][:motto])
 
-      "#{params.class}"
+
+      Member.new(params[:team][:members][0][:name], params[:team][:members][0][:power], params[:team][:members][0][:bio])
+      Member.new(params[:team][:members][1][:name], params[:team][:members][1][:power], params[:team][:members][1][:bio])
+      Member.new(params[:team][:members][2][:name], params[:team][:members][2][:power], params[:team][:members][2][:bio])
+
+      @members = Member.all
+
+      erb :team
+
+      # "#{params.class}"
     end
 
 
