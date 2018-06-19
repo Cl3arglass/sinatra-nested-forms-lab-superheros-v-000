f4ed9f76f90c1fb232f7505ccd05b1bbf370e3c1
Holden Altaffer <haltaffer@gmail.com>
2018-06-19 00:23:34 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,14 +9,14 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
-      # @team = Team.new(params[:team][:name], params[:team][:motto])
-      #
-      #
-      # Member.new(params[:team][:members][0][:name], params[:team][:members][0][:power], params[:team][:members][0][:bio])
-      # Member.new(params[:team][:members][1][:name], params[:team][:members][1][:power], params[:team][:members][1][:bio])
-      # Member.new(params[:team][:members][2][:name], params[:team][:members][2][:power], params[:team][:members][2][:bio])
-      #
-      # @members = Member.all
+      @team = Team.new(params[:team][:name], params[:team][:motto])
+
+
+      Member.new(params[:team][:members][0][:name], params[:team][:members][0][:power], params[:team][:members][0][:bio])
+      Member.new(params[:team][:members][1][:name], params[:team][:members][1][:power], params[:team][:members][1][:bio])
+      Member.new(params[:team][:members][2][:name], params[:team][:members][2][:power], params[:team][:members][2][:bio])
+
+      @members = Member.all
 
       erb :team
 
